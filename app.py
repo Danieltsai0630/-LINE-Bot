aecbb471395db59aefda79f67bbb79788df4b9c2
Danieltsai0630 <66794222+Danieltsai0630@users.noreply.github.com>
2021-07-05 05:19:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,3 +38,14 @@ def handle_message(event):
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
+@app.join(MessageEvent, message=TextMessage)
+protected override async Task OnJoinAsync(JoinEvent ev)
+{
+    await _messagingClient.ReplyMessageAsync(ev.ReplyToken,
+        $"OnJoin 加入事件\n" +
+        $"類型: {ev.Type.ToString()}\n" +
+        $"時間: {ev.Timestamp}\n" +
+        $"來源類型: {ev.Source.Type.ToString()}\n" +
+        $"頻道 ID: {ev.Source.Id}\n" +
+        $"用戶 ID: {ev.Source.UserId}");
+}
